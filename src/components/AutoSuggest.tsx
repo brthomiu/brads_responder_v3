03ab676aaa0f3077de360a712947aed882eb9139
thompsonbrad <thompsonbrad@google.com>
2025-10-31 15:24:53 +0000

    commenting out unfinished features so that the project will build

--- src/components/AutoSuggest.tsx
+++ src/components/AutoSuggest.tsx
@@ -1,14 +1,14 @@
-type Props = {inputText: string; itemNameSuggestions: string[]};
+// type Props = {inputText: string; itemNameSuggestions: string[]};
 
-export function AutoSuggest({inputText, itemNameSuggestions}: Props) {
+// export function AutoSuggest({inputText, itemNameSuggestions}: Props) {
 
-  return (
-    <div>
-          {itemNameSuggestions.map((suggestion) => (
-            <div key={suggestion} className="p-2 hover:bg-gray-700 cursor-pointer">
-              {suggestion}
-            </div>
-          ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//           {itemNameSuggestions.map((suggestion) => (
+//             <div key={suggestion} className="p-2 hover:bg-gray-700 cursor-pointer">
+//               {suggestion}
+//             </div>
+//           ))}
+//     </div>
+//   );
+// }
